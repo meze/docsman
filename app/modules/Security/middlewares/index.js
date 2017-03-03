d16957f5f672bd32071efd73ebf3cc520cfcaf28
Evgeniy Z <mz.evgeny@gmail.com>
2017-03-03 06:06:52 +0300

    Fix security middleware not returning the result

--- app/modules/Security/middlewares/index.js
+++ app/modules/Security/middlewares/index.js
@@ -4,10 +4,10 @@ import { logout } from '../actions/handlers';
 import type { ActionType } from '../../../types/redux';
 
 export default (store: Object) => (next: Function) => (action: ActionType) => {
+  const result = next(action);
   if (action.error && action.error.status === 401) {
-    next(action);
     store.dispatch(logout());
-  } else {
-    next(action);
   }
+
+  return result;
 };
